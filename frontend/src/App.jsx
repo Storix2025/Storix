17859fb0261481eaf0fbec26069154df77bb7531
Неупокоев Дмитрий <127975789+NeuDima@users.noreply.github.com>
2025-05-06 06:32:28 +0300

    delete unnecessary comments

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,8 +8,6 @@ import InventarizationPage from "./pages/InventariztionPage";
 export default function App() {
   return (
     <>
-      {/* {location.pathname === "pages" && <NavBar />} */}
-
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
@@ -17,12 +15,7 @@ export default function App() {
           <Route path="/pricing" element={<Pricing />} />
           <Route path="/histoy-check" />
           
-          {/* <Route path="/layout" element={<Layout />}> */}
           <Route path="/inventarization" element={<InventarizationPage />} />
-          {/* <Route path="store-history" element={<HistoryPage />} /> */}
-            {/* <Route path="store-history" element={<StoreHistory />} />
-            <Route path="store-history" element={<StoreHistory />} /> */}
-          
         </Routes>
       </BrowserRouter>
     </>
