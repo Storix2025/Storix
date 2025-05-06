ef9eaca0a3ab762a156660f47dd97995870b962e
Неупокоев Дмитрий <127975789+NeuDima@users.noreply.github.com>
2025-05-06 06:31:58 +0300

    delete unnecessary comments

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,45 +1,17 @@
 import styles from "./Home.module.css";
-// import logoIcon from "../../assets/image/logo-icon.png";
 import alarmClock from "../../assets/image/alarm-clock.png";
 import bell from "../../assets/image/bell.png";
 import checkMark from "../../assets/image/grayCheckMark.png";
 import { useNavigate } from "react-router-dom";
 import Logo from "../../components/Logo";
-// import { useEffect, useState } from "react";
 
 export default function Home() {
   const navigate = useNavigate();
-  // const [data, setData] = useState(null); // Сюда сохраняется результат
-  // const [error, setError] = useState(null); // Для обработки ошибок
-  // console.log("data :>> ", data);
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       const res = await fetch("https://storix.onrender.com/api/inventory/reports/");
-
-  //       if (!res.ok) {
-  //         throw new Error(`Ошибка сервера: ${res.status}`);
-  //       }
-
-  //       const result = await res.json(); // или .text() если не JSON
-  //       setData(result);
-  //     } catch (err) {
-  //       console.error("Ошибка запроса:", err);
-  //       setError(err.message);
-  //     }
-  //   };
-  //   console.error("Ошибка запроса:", error);
-  //   fetchData();
-  // }, []);
   return (
     <div className={styles.background}>
       <div className={styles.container}>
         <header className={styles.header}>
           <Logo />
-          {/* <div className={styles.logoBox}>
-            <img src={logoIcon} alt="Storix" />
-            <span className={styles.logoText}>Storix</span>
-          </div> */}
           <button
             className={styles.loginBtn}
             onClick={() => navigate("/login")}
@@ -81,9 +53,6 @@ export default function Home() {
           </div>
         </main>
       </div>
-      {/* <button className={styles.nav} onClick={() => navigate("/layout")}>
-        Страницы
-      </button> */}
     </div>
   );
 }
